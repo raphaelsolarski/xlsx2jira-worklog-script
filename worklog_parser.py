f12b2b7a9812c3c58b4a488ce728668243507826
Rafał Solarski <solarski.rafal@gmail.com>
2025-07-11 13:25:42 +0200

    Update worklog_parser.py
    comment moved to 6th column

--- worklog_parser.py
+++ worklog_parser.py
@@ -8,11 +8,11 @@ def parse_input_file(file_name):
     rows = []
     wb = openpyxl.load_workbook(file_name)
     ws = wb.active
-    for (index, row) in enumerate(ws.iter_rows(min_row=2, max_col=5)):
+    for (index, row) in enumerate(ws.iter_rows(min_row=2, max_col=6)):
         date = row[0].value
         task_id = row[1].value
         hours = row[2].value or None
         in_jira = row[3].value != 'N'
-        comment = row[4].value
+        comment = row[5].value
         rows.append(WorklogRecord(index + 2, date, task_id, hours, in_jira, comment))
     return rows
